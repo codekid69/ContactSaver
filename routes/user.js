c7342b484cab922ca6c5ef88f51b817fccbb9a4b
codekid69 <bishtyash069@gmail.com>
2023-03-11 16:03:03 +0530

    FInally done

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,6 @@ router.get('/sign-in',user.signin);
 router.get('/logout',passport.checkAuthentication,user.destroySession);
 router.get('/delete/:id',passport.checkAuthentication,user.delete);
 router.post('/create-user',user.createUser);
-router.post('/create-session',passport.authenticate('local',{failureRedirect:'/sign-in'}),user.createSession);
+router.post('/create-session',passport.authenticate('local',{failureRedirect:'/user/sign-in'}),user.createSession);
 router.post('/add-contact',passport.checkAuthentication,user.createContact);
 module.exports=router;
