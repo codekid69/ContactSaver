e556390eb21d37aa192ade86f3a17e6bd80f5574
codekid69 <bishtyash069@gmail.com>
2023-03-11 12:30:05 +0530

    EDITS

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const app=express();
-require('dotenv').config();
-const port=process.env.BASE_URL || 8000;
+const port=process.env.PORT || 8000;
 const db=require('./config/mongoose');
 const cookie=require('cookie-parser');
 const session=require('express-session');
