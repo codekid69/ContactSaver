8faff090fe98c65b105ed7ca060ec947842dc009
codekid69 <bishtyash069@gmail.com>
2023-03-15 20:05:57 +0530

    Final Commit

--- config/passport-Google.js
+++ config/passport-Google.js
@@ -6,43 +6,38 @@ require('dotenv').config();
 // using the google strategy
 passport.use(new googleStrategy({
     clientID: process.env.ID,
-    clientSecret:process.env.SECERET,
+    clientSecret: process.env.SECRET,
     callbackURL: 'https://contactsaver.onrender.com/user/auth/google/callback'
 },
 
     // if acesstoken expries then then refreshtoken get the new one without asking the  user to login
-    function (accessToken, refreshToken, profile, done) {
-
-        // finding the user in db 
-        User.findOne({ email: profile.emails[0].value }).exec(function (error, user) {
-            if (error) {
-                console.log(error);
-                return;
-            }
+    async function (accessToken, refreshToken, profile, done) {
+        try {
+            // finding the user in db 
+            let user = await User.findOne({ email: profile.emails[0].value });
             console.log(profile);
-
             // if user  found set it to req.user
-            if(user){
-                return done(null,user);
+            if (user) {
+                return done(null, user);
             }
-
             // if not then create the user
-            else{
-                User.create({
-                    name:profile.displayName,
-                    email:profile.emails[0].value,
-                    password:crypto.randomBytes(20).toString('hex')
-                },function(error,user){
-                    if(error){
-                        console.log(error);
-                        return;
-                    }
-                    return done(null,user);
-                })
+            else {
+                let user = await User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                });
+                try {
+                    user.save();
+                } catch (error) {
+                    console.log(error);
+                }
             }
-        })
+        }catch(error){
+            console.log(error);
+        }  
     }
 
 ))
 
-module.exports=passport;
+module.exports = passport;
